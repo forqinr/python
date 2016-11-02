4adc32399fc8384db69b75b168999e0a032595f6
DreamFor5000000 <wdhylpf@163.com>
2016-11-02 08:53:57 +0800

    formate

--- makeTextFile.py
+++ makeTextFile.py
@@ -3,18 +3,18 @@
 'makeTextFile.py -- create text file'
 
 import os
+
 ls = os.linesep
 
-#get filename
+# get filename
 while True:
     fname = raw_input('input file name:')
     if os.path.exists(fname):
-        print "ERROR:'%s' already exists" %fname
+        print "ERROR:'%s' already exists" % fname
     else:
         break
 
-
-#get file content (text) lines
+# get file content (text) lines
 all = []
 print "\nEnter lines ('.' by itself to quit).\n"
 
@@ -27,6 +27,6 @@ while True:
 
 # write lines to file with proper line-ending
 fobj = open(fname, 'w')
-fobj.writelines(['%s%s' % (x,ls) for x in all])
+fobj.writelines(['%s%s' % (x, ls) for x in all])
 fobj.close()
 print 'DONE!'
